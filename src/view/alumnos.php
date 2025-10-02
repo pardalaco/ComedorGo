8b3e1a96529133af9128fda975d9f4109a5f8339
Daniel Rovira Martínez <pardalaco@gmail.com>
2025-10-02 13:20:33 +0200

    alumnos: assets js y css en local

--- src/view/alumnos.php
+++ src/view/alumnos.php
@@ -18,20 +18,20 @@ $activePage = 'alumnos'; // Para resaltar la página activa en el sidebar
 
     <!-- Tabla -->
     <!-- DataTables CSS -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
+    <link rel="stylesheet" href="../assets/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="../assets/css/responsive.bootstrap5.min.css">
 
 </head>
 <!--end::Head-->
 
-<!-- jQuery (AdminLTE ya lo usa, pero si no lo tienes, añádelo) -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- jQuery -->
+<script src="../assets/js/jquery-3.6.0.min.js"></script>
 
 <!-- DataTables JS -->
-<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"></script>
+<script src="../assets/js/jquery.dataTables.min.js"></script>
+<script src="../assets/js/dataTables.bootstrap5.min.js"></script>
+<script src="../assets/js/dataTables.responsive.min.js"></script>
+<script src="../assets/js/responsive.bootstrap5.min.js"></script>
 
 
 <!--begin::Body-->
